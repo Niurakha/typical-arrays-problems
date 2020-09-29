309c7926f2cc406ea36093c4c8951cc88ad01c7e
Niurakha <proshinaann89@gmail.com>
2020-09-29 15:56:31 +0300

    feat: add task typical-arrays-problems solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+    else {
+        let min = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (min > array[i]) min = array[i];
+        }
+        return min;
+    }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let max = array[0];
+        for (let i = 0; i < array.length; i++) {
+            if (max < array[i]) max = array[i];
+        }
+        return max;
+    }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    } else {
+        let avg = 0;
+        for (let i = 0; i < array.length; i++) {
+            avg += array[i];
+        }
+        return avg / array.length;
+    }
 }
